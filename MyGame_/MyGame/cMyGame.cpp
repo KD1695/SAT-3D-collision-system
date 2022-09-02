3e0cb4a60a6031f66a3d56b47d0c5d7569a60098
KD <kirtidhwajsingh95@gmail.com>
2022-09-01 22:59:18 -0600

    added init and cleanup message logs

--- MyGame_/MyGame/cMyGame.cpp
+++ MyGame_/MyGame/cMyGame.cpp
@@ -5,6 +5,7 @@
 
 #include <Engine/Asserts/Asserts.h>
 #include <Engine/UserInput/UserInput.h>
+#include <Engine/Logging/Logging.h>
 
 // Inherited Implementation
 //=========================
@@ -28,10 +29,12 @@ void eae6320::cMyGame::UpdateBasedOnInput()
 
 eae6320::cResult eae6320::cMyGame::Initialize()
 {
+	eae6320::Logging::OutputMessage("Initializing Game...");
 	return Results::Success;
 }
 
 eae6320::cResult eae6320::cMyGame::CleanUp()
 {
+	eae6320::Logging::OutputMessage("Cleaning up Game...");
 	return Results::Success;
 }

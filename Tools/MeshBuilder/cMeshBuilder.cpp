2e3f9458020f25fbe092c7e16505ae75e74c7f96
KD <kirtidhwajsingh95@gmail.com>
2022-10-28 12:14:32 -0600

    fix issues with writing to binary file

--- Tools/MeshBuilder/cMeshBuilder.cpp
+++ Tools/MeshBuilder/cMeshBuilder.cpp
@@ -25,16 +25,52 @@ eae6320::cResult eae6320::Assets::cMeshBuilder::Build(const std::vector<std::str
 	if (itr != j.end())
 	{
 		auto _vertexData = j["vertexPositionData"].get<std::vector<std::vector<float>>>();
-		size_t len = _vertexData.size();
-		fwrite((void*)&len, sizeof(size_t), 1, fileToWrite);
-		fwrite((void*)_vertexData.data(), sizeof(_vertexData[0]), _vertexData.size(), fileToWrite);
+		uint16_t len = (uint16_t)_vertexData.size();
+		fwrite((void*)&len, sizeof(uint16_t), 1, fileToWrite);
+		float* vertexDataArray = (float*)calloc(len, sizeof(float) * 3);
+		float* floatIterator = vertexDataArray;
+		for (size_t i = 0; i < _vertexData.size(); i++)
+		{
+			*floatIterator = _vertexData[i][0];
+			floatIterator++;
+			*floatIterator = _vertexData[i][1];
+			floatIterator++;
+			*floatIterator = _vertexData[i][2];
+			floatIterator++;
+		}
+		fwrite(vertexDataArray, sizeof(float), _vertexData.size() * 3, fileToWrite);
+		free(vertexDataArray);
 	}
+	
+	auto itr3 = j.find("vertexColorData");
+	if (itr3 != j.end())
+	{
+		auto _vertexColorData = j["vertexColorData"].get<std::vector<std::vector<float>>>();
+		uint16_t len = (uint16_t)_vertexColorData.size();
+		fwrite((void*)&len, sizeof(uint16_t), 1, fileToWrite);
+		float* vertexColorDataArray = (float*)calloc(len, sizeof(float) * 4);
+		float* floatIterator = vertexColorDataArray;
+		for (size_t i = 0; i < _vertexColorData.size(); i++)
+		{
+			*floatIterator = _vertexColorData[i][0];
+			floatIterator++;
+			*floatIterator = _vertexColorData[i][1];
+			floatIterator++;
+			*floatIterator = _vertexColorData[i][2];
+			floatIterator++;
+			*floatIterator = _vertexColorData[i][3];
+			floatIterator++;
+		}
+		fwrite(vertexColorDataArray, sizeof(float), _vertexColorData.size() * 4, fileToWrite);
+		free(vertexColorDataArray);
+	}
+
 	auto itr2 = j.find("indexData");
 	if (itr2 != j.end())
 	{
-		auto _indexData = j["indexData"].get<std::vector<float>>();
-		size_t len = _indexData.size();
-		fwrite((void*)&len, sizeof(size_t), 1, fileToWrite);
+		auto _indexData = j["indexData"].get<std::vector<uint16_t>>();
+		uint16_t len = (uint16_t)_indexData.size();
+		fwrite((void*)&len, sizeof(uint16_t), 1, fileToWrite);
 		fwrite((void*)_indexData.data(), sizeof(_indexData[0]), _indexData.size(), fileToWrite);
 	}
 
